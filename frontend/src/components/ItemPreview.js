be4c213c1c7be81b25f45cc384419aede3f98cfd
Alvaro Clemente <clemente.varo@gmail.com>
2022-07-27 21:56:47 +0200

    fix: make test pass

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -28,6 +28,7 @@ const ItemPreview = (props) => {
       props.favorite(item.slug);
     }
   };
+  const image = item.image || "/placeholder.png";
 
   return (
     <div
@@ -36,7 +37,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image}
+        src={image}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
